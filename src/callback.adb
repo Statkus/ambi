e987abcbfb8e8a2183c93a7e6fdedc7824931146
Léo Statkus <leo@sogilis.com>
2019-02-01 23:23:04 +0100

    [DEV] First implementation of video search list response parser.

--- src/callback.adb
+++ src/callback.adb
@@ -1,6 +1,5 @@
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-with Ada.Text_IO;           use Ada.Text_IO;
-with Ada.Strings.Fixed;     use Ada.Strings.Fixed;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+with Ada.Text_IO;       use Ada.Text_IO;
 
 with AWS.Client;
 with AWS.MIME;
@@ -30,28 +29,27 @@ package body Callback is
         & "&part=snippet&q=" & Search_Input & "&videoDefinition=high&type=video&order=viewCount";
       API_Result  : AWS.Response.DATA;
 
-      Video_ID_Search_Result : T_Video_ID_Search_Result := (others => (others => '0'));
+      Video_Search_List_Response : T_Video_Search_List_Response;
    begin
       Put_Line ("NEW REQUEST ----------------------------------------------------------------");
       Put_Line ("API query: " & API_Request);
       API_Result := AWS.Client.Get (URL => API_Request);
-      Put_Line ("API result:");
-      Put_Line (AWS.Response.Message_Body (API_Result));
+      --Put_Line ("API result:");
+      --Put_Line (AWS.Response.Message_Body (API_Result));
 
 
-      Video_ID_Search_Result := Parse_Search_Request (AWS.Response.Message_Body (API_Result));
+      Video_Search_List_Response := Parse_Video_Search_List_Request (AWS.Response.Message_Body (API_Result));
 
       for Index in 1 .. YT_VIDEO_SEARCH_RESULT loop
-         Put_Line (Video_ID_Search_Result (Index));
+         Put_Line (To_String (Video_Search_List_Response.Video_Search_Results (Index).Video_Title));
       end loop;
 
       if URI = "/search_result" then
-         Put_Line ("coucou");
          return AWS.Response.Build (AWS.MIME.Text_HTML,
-                                    Build_Search_Result (Video_ID_Search_Result));
+                                    Build_Search_Result (Video_Search_List_Response));
       end if;
 
-      Video_ID := Video_ID_Search_Result (Video_ID_Search_Result'First);
+      Video_ID := Video_Search_List_Response.Video_Search_Results (1).Video_ID;
 
       Put_Line ("Search input: " & Search_Input);
       Put_Line ("Video ID: " & Video_ID);
@@ -73,45 +71,136 @@ package body Callback is
    end Set_YT_API_Key;
 
    -------------------------------------------------------------------------------------------------
-   -- Parse_Search_Request
+   -- Parse_Video_Search_List_Request
    -------------------------------------------------------------------------------------------------
-   function Parse_Search_Request (Search_Result : in String) return T_Video_ID_Search_Result is
-      Video_ID_Pattern : constant GNAT.Regpat.Pattern_Matcher :=
-        GNAT.Regpat.Compile ("""videoId"": ""([a-zA-Z0-9_-]+)""");
+   function Parse_Video_Search_List_Request (Search_Result : in String)
+     return T_Video_Search_List_Response is
 
-      REGEX_Search_Offset : Natural := 0;
+      Next_Page_Token  : String (1 .. YT_PAGE_TOKEN_LENGTH) := (others => '0');
+      Total_Results    : Integer := 0;
+      Results_Per_Page : Integer := 0;
 
-      Result : GNAT.Regpat.Match_Array (0 .. 1);
+      Video_Search_Results : T_Video_Search_Results;
+      Video_Search_Offset : Natural := 0;
 
-      Video_ID_Search_Result : T_Video_ID_Search_Result := (others => (others => '0'));
+      Field_Not_Found : Exception;
    begin
-      for Index in Video_ID_Search_Result'Range loop
-         GNAT.Regpat.Match (Video_ID_Pattern, Search_Result
-           (Search_Result'First + REGEX_Search_Offset .. Search_Result'Last), Result);
+      -- Parse next page token
+      Next_Page_Token := REGEX_Match_To_String
+        ("""nextPageToken"": ""([A-Z]+)""", Search_Result, Video_Search_Offset);
 
-         if Result (1) /= GNAT.Regpat.No_Match then
-            Put_Line ("Match :" & Search_Result (Result (1).First .. Result (1).Last));
-            Video_ID_Search_Result (Index)
-              (Video_ID_Search_Result (Index)'First .. Video_ID_Search_Result (Index)'Last) :=
-              Search_Result (Result (1).First .. Result (1).Last);
+      if Next_Page_Token = "" then
+         raise Field_Not_Found with "Next page token not found";
+      end if;
 
-            REGEX_Search_Offset := Result (1).Last;
+      -- Parse total results
+      Total_Results := REGEX_Match_To_Integer
+        ("""totalResults"": ([0-9]+)", Search_Result, Video_Search_Offset);
+
+      if Total_Results = Integer'First then
+         raise Field_Not_Found with "Total results not found";
+      end if;
+
+      -- Parse result per page
+      Results_Per_Page := REGEX_Match_To_Integer
+        ("""resultsPerPage"": ([0-9]+)", Search_Result, Video_Search_Offset);
+
+      if Results_Per_Page = Integer'First then
+         raise Field_Not_Found with "Results per page not found";
+      end if;
+
+      -- Parse video search results
+      for Index in 1 .. Results_Per_Page loop
+         Video_Search_Results (Index).Video_ID :=
+           REGEX_Match_To_String ("""videoId"": ""([a-zA-Z0-9_-]+)""", Search_Result
+               (Search_Result'First + Video_Search_Offset .. Search_Result'Last),
+             Video_Search_Offset);
+
+         Video_Search_Results (Index).Video_Title :=
+           To_Unbounded_String (
+             REGEX_Match_To_String ("""title"": ""(.+)"",", Search_Result
+                 (Search_Result'First + Video_Search_Offset .. Search_Result'Last),
+               Video_Search_Offset));
+
+         if Video_Search_Results (Index).Video_Title = "" then
+           raise Field_Not_Found with "Video title not found";
+         end if;
+
+         Video_Search_Results (Index).Video_Image_URL :=
+           To_Unbounded_String (
+             REGEX_Match_To_String
+               ("""url"": ""(https://i.ytimg.com/vi/[a-zA-Z0-9_-]+/default.jpg)"",",
+                Search_Result
+                  (Search_Result'First + Video_Search_Offset .. Search_Result'Last),
+                Video_Search_Offset));
+
+         if Video_Search_Results (Index).Video_Image_URL = "" then
+           raise Field_Not_Found with "Video image URL not found";
          end if;
       end loop;
 
-      return Video_ID_Search_Result;
-   end Parse_Search_Request;
+      return (Next_Page_Token, Total_Results, Results_Per_Page, Video_Search_Results);
+   end Parse_Video_Search_List_Request;
 
-   function Build_Search_Result
-     (Video_ID_Search_Result : in T_Video_ID_Search_Result) return String is
-      Translations : Templates_Parser.Translate_Table (Video_ID_Search_Result'Range);
+   -------------------------------------------------------------------------------------------------
+   -- REGEX_Match_To_String
+   -------------------------------------------------------------------------------------------------
+   function REGEX_Match_To_String
+     (Pattern : in String;
+      Source  : in String;
+      Offset  : out Integer)
+     return String is
+      Match_Pattern : GNAT.Regpat.Pattern_Matcher := GNAT.Regpat.Compile (Pattern);
+      Match_Result  : GNAT.Regpat.Match_Array (0 .. 1);
    begin
-      for Index in Video_ID_Search_Result'Range loop
-         Translations (Index) := Templates_Parser.Assoc
-           ("VIDEO_ID_" & Trim (Index'Img, Ada.Strings.Left), Video_ID_Search_Result (Index));
-      end loop;
+      Offset := Source'First;
 
-      Put_Line (Templates_Parser.Parse ("html/search_result.thtml", Translations));
+      GNAT.Regpat.Match (Match_Pattern, Source, Match_Result);
+
+      if Match_Result (1) /= GNAT.Regpat.No_Match then
+         Offset := Match_Result (1).Last;
+         return Source (Match_Result (1).First .. Match_Result (1).Last);
+      else
+         return "";
+      end if;
+   end REGEX_Match_To_String;
+
+   -------------------------------------------------------------------------------------------------
+   -- REGEX_Match_To_Integer
+   -------------------------------------------------------------------------------------------------
+   function REGEX_Match_To_Integer
+     (Pattern : in String;
+      Source  : in String;
+      Offset  : out Integer)
+     return Integer is
+      Match_Pattern : GNAT.Regpat.Pattern_Matcher := GNAT.Regpat.Compile (Pattern);
+      Match_Result  : GNAT.Regpat.Match_Array (0 .. 1);
+   begin
+      Offset := Source'First;
+
+      GNAT.Regpat.Match (Match_Pattern, Source, Match_Result);
+
+      if Match_Result (1) /= GNAT.Regpat.No_Match then
+         Offset := Match_Result (1).Last;
+         return Integer'Value (Source (Match_Result (1).First .. Match_Result (1).Last));
+      else
+         return Integer'First;
+      end if;
+   end REGEX_Match_To_Integer;
+
+   -------------------------------------------------------------------------------------------------
+   -- Build_Search_Result
+   -------------------------------------------------------------------------------------------------
+   function Build_Search_Result
+     (Video_Search_List_Response : in T_Video_Search_List_Response) return String is
+      Translations : Templates_Parser.Translate_Table
+        (1 .. Video_Search_List_Response.Results_Per_Page);
+   begin
+      for Index in 1 .. Video_Search_List_Response.Results_Per_Page loop
+         Translations (Index) := Templates_Parser.Assoc
+           ("IMAGE_URL_" & Trim (Index'Img, Ada.Strings.Left),
+            Video_Search_List_Response.Video_Search_Results (Index).Video_Image_URL);
+      end loop;
 
       return Templates_Parser.Parse ("html/search_result.thtml", Translations);
    end Build_Search_Result;
--- src/callback.ads
+++ src/callback.ads
@@ -1,3 +1,5 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 with AWS.Response;
 with AWS.Status;
 
@@ -14,13 +16,41 @@ private
 
    YT_VIDEO_ID_LENGTH     : constant := 11;
    YT_VIDEO_SEARCH_RESULT : constant := 5;
+   YT_PAGE_TOKEN_LENGTH   : constant := 6;
 
-   type T_Video_ID_Search_Result is array (1 .. YT_VIDEO_SEARCH_RESULT) of
-     String (1 .. YT_VIDEO_ID_LENGTH);
+   type T_Video_Search_Result is record
+      Video_ID           : String (1 .. YT_VIDEO_ID_LENGTH) := (others => '0');
+      Video_Title        : Unbounded_String;
+      Video_Image_URL    : Unbounded_String;
+      Video_Image_Width  : String (1 .. 3) := "320";
+      VIdeo_Image_Height : String (1 .. 3) := "180";
+   end record;
 
-   function Parse_Search_Request (Search_Result : in String) return T_Video_ID_Search_Result;
+   type T_Video_Search_Results is array (1 .. YT_VIDEO_SEARCH_RESULT) of T_Video_Search_Result;
+
+   type T_Video_Search_List_Response is record
+      Next_Page_Token      : String (1 .. YT_PAGE_TOKEN_LENGTH) := (others => '0');
+      Total_Results        : Natural := 0;
+      Results_Per_Page     : Natural := 0;
+      Video_Search_Results : T_Video_Search_Results;
+   end record;
+
+   function Parse_Video_Search_List_Request (Search_Result : in String)
+     return T_Video_Search_List_Response;
+
+   function REGEX_Match_To_String
+     (Pattern : in String;
+      Source  : in String;
+      Offset  : out Integer)
+     return String;
+
+   function REGEX_Match_To_Integer
+     (Pattern : in String;
+      Source  : in String;
+      Offset  : out Integer)
+     return Integer;
 
    function Build_Search_Result
-     (Video_ID_Search_Result : in T_Video_ID_Search_Result) return String;
+     (Video_Search_List_Response : in T_Video_Search_List_Response) return String;
 
 end Callback;

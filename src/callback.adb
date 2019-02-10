a2136db78874bc0f03b77b5018b5b562c89edb4c
Léo Statkus <leo@sogilis.com>
2019-02-10 18:33:54 +0100

    [DEV] Functionnal youtube player with search bar.

--- src/callback.adb
+++ src/callback.adb
@@ -20,31 +20,20 @@ package body Callback is
    -------------------------------------------------------------------------------------------------
    function Ambi_Callback (Request : AWS.Status.Data) return AWS.Response.Data is
       URI        : constant String := AWS.Status.URI (Request);
-      Parameters : constant AWS.Parameters.List := AWS.Status.Parameters (Request);
 
       Translations : Templates_Parser.Translate_Table (1 .. 1);
 
       Web_Page : Unbounded_String;
    begin
-      Put_Line ("New Ambi callback --------------------------------------------------------------");
       Put_Line (URI);
 
-      if URI = "/search_result" then
-         return Search_Result_Callback (Request);
-      elsif URI = "/1" then
-         Current_Room.Set_Current_Video (1);
-      elsif URI = "/2" then
-         Current_Room.Set_Current_Video (2);
-      elsif URI = "/3" then
-         Current_Room.Set_Current_Video (3);
-      elsif URI = "/4" then
-         Current_Room.Set_Current_Video (4);
-      elsif URI = "/5" then
-         Current_Room.Set_Current_Video (5);
-      elsif URI = "/onclick$clickme" then
-         return AWS.Response.Build (AWS.MIME.Text_HTML, "you click me!");
-      elsif Index (URI, "we_js") > 0 then
+      if Index (URI, "we_js") > 0 then
          return Javascripts_Callback (Request);
+      elsif URI = "/onclick$search_button" then
+         return Search_Button_Callback (Request);
+      elsif Index (URI, "/onclick$search_result_item_") > 0 then
+         Current_Room.Set_Current_Video (Integer'Value (URI (URI'Last .. URI'Last)));
+         return AWS.Response.Build (AWS.MIME.Text_HTML, "");
       end if;
 
       Put_Line ("Video to play: " & To_String (Current_Room.Get_Current_Video.Video_Title));
@@ -73,8 +62,6 @@ package body Callback is
 
       Match_Result : GNAT.Regpat.Match_Array (0 .. 1);
    begin
-      Put_Line ("New Javascripts callback -------------------------------------------------------");
-
       GNAT.Regpat.Match (JS_Match_Pattern, URI, Match_Result);
 
       if Match_Result (1) /= GNAT.Regpat.No_Match then
@@ -95,29 +82,24 @@ package body Callback is
    end Javascripts_Callback;
 
    -------------------------------------------------------------------------------------------------
-   -- Search_Result_Callback
+   -- Search_Button_Callback
    -------------------------------------------------------------------------------------------------
-   function Search_Result_Callback (Request : AWS.Status.Data) return AWS.Response.Data is
-      Parameters  : constant AWS.Parameters.List := AWS.Status.Parameters (Request);
+   function Search_Button_Callback (Request : AWS.Status.Data) return AWS.Response.Data is
+      Parameters   : constant AWS.Parameters.List := AWS.Status.Parameters (Request);
       Search_Input : constant String := AWS.Parameters.Get (Parameters, "search_input");
 
       API_Request  : String := Get_Search_Request (Search_Input);
       API_Response : AWS.Response.DATA;
    begin
-      Put_Line ("New Search_Result callback -----------------------------------------------------");
       Put_Line ("API query: " & API_Request);
       API_Response := AWS.Client.Get (URL => API_Request);
 
       Current_Room.Set_Video_Search_Results
         (Parse_Video_Search_Results (AWS.Response.Message_Body (API_Response)));
 
-      for Index in Current_Room.Get_Video_Search_Results'Range loop
-         Put_Line (To_String (Current_Room.Get_Video_Search_Results (Index).Video_Title));
-      end loop;
-
       return AWS.Response.Build (AWS.MIME.Text_HTML,
                                  Build_Search_Result (Current_Room.Get_Video_Search_Results));
-   end Search_Result_Callback;
+   end Search_Button_Callback;
 
    -------------------------------------------------------------------------------------------------
    -- Build_Search_Result
@@ -125,19 +107,26 @@ package body Callback is
    function Build_Search_Result
      (Video_Search_Results : in T_Video_Search_Results) return String is
       Translations : Templates_Parser.Translate_Table
-        (1 .. Current_Room.Get_Video_Search_Results'Length * 2);
+        (1 .. 3);
+      Response : Unbounded_String := To_Unbounded_String ("<ul>");
    begin
-      for Index in Current_Room.Get_Video_Search_Results'Range loop
-         Translations (Index) := Templates_Parser.Assoc
-           ("IMAGE_URL_" & Trim (Index'Img, Ada.Strings.Left),
-            To_String (Current_Room.Get_Video_Search_Results (Index).Video_Image_URL));
-         Translations (Current_Room.Get_Video_Search_Results'Length + Index) :=
-           Templates_Parser.Assoc
-             ("VIDEO_TITLE_" & Trim (Index'Img, Ada.Strings.Left),
-              To_String (Current_Room.Get_Video_Search_Results (Index).Video_Title));
+      for Result_Index in Video_Search_Results'Range loop
+         Translations (1) := Templates_Parser.Assoc
+           ("ITEM_ID", Trim (Result_Index'Img, Ada.Strings.Left));
+
+         Translations (2) := Templates_Parser.Assoc
+           ("IMAGE_URL", To_String (Video_Search_Results (Result_Index).Video_Image_URL));
+
+         Translations (3) := Templates_Parser.Assoc
+           ("VIDEO_TITLE", To_String (Video_Search_Results (Result_Index).Video_Title));
+
+         Append (Response, To_String
+           (Templates_Parser.Parse ("html/search_results_list_item.thtml", Translations)));
       end loop;
 
-      return Templates_Parser.Parse ("html/search_result.thtml", Translations);
+      Append (Response, "</ul>");
+
+      return To_String (Response);
    end Build_Search_Result;
 
 end Callback;
--- src/callback.ads
+++ src/callback.ads
@@ -13,7 +13,7 @@ private
 
    function Javascripts_Callback (Request : AWS.Status.Data) return AWS.Response.Data;
 
-   function Search_Result_Callback (Request : AWS.Status.Data) return AWS.Response.Data;
+   function Search_Button_Callback (Request : AWS.Status.Data) return AWS.Response.Data;
 
    function Build_Search_Result
      (Video_Search_Results : in T_Video_Search_Results) return String;

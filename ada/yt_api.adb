ae9d7b6c6f85d4d943c5dab656c2449ee5b92039
Léo Statkus <leo@sogilis.com>
2019-03-25 10:52:06 +0100

    [FIX] Change number of results un related vdeo search.

--- ada/yt_api.adb
+++ ada/yt_api.adb
@@ -78,7 +78,7 @@ package body YT_API is
    begin
       return YT_API_URL & "search?key=" & To_String (YT_API_KEY)
         & "&relatedToVideoId=" & Video_ID
-        & "&maxResults=7"
+        & "&maxResults=4"
         & "&part=snippet&videoDefinition=any&type=video&safeSearch=none&videoSyndicated=true";
    end Get_Videos_Related_Request;
 
